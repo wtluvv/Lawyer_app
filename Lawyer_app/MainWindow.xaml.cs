40d5b9f5e46911de362829a8995835869f15ab7b
wtluvv <korimyy@gmail.com>
2025-06-08 19:58:06 +0300

    fix: correct XAML namespaces and connect Task list to MySQL

--- Lawyer_app/MainWindow.xaml.cs
+++ Lawyer_app/MainWindow.xaml.cs
@@ -1,24 +1,23 @@
-﻿using System.Text;
+﻿using Lawyer_app.Services;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Lawyer_app
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly DbHelper _dbHelper;
+
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent(); // Важно: должна быть первой строкой!
+            _dbHelper = new DbHelper("Server=localhost;Database=lawyer_app;Uid=root;Pwd=;");
+            LoadTasks();
+        }
+
+        private void LoadTasks()
+        {
+            var tasks = _dbHelper.GetTasks();
+            TasksListView.ItemsSource = tasks; // Имя должно совпадать с x:Name в XAML
         }
     }
 }

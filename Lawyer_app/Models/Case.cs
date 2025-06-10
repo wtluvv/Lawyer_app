b8a8043cc3871c32bb2504c042c203cfb2d2958b
wtluvv <korimyy@gmail.com>
2025-06-10 21:34:40 +0300

    add: add addcasewindow, client, update models

---
+++ Lawyer_app/Models/Case.cs
@@ -0,0 +1,27 @@
+﻿using Postgrest.Attributes;
+using Postgrest.Models;
+
+namespace Lawyer_app.Models
+{
+    [Table("cases")]
+    public class Case : BaseModel
+    {
+        [PrimaryKey("id", false)]
+        public int Id { get; set; }
+
+        [Column("case_number")]
+        public string CaseNumber { get; set; }
+
+        [Column("client_id")]
+        public int ClientId { get; set; }
+
+        [Column("status")]
+        public string Status { get; set; }
+
+        [Column("description")]
+        public string Description { get; set; }
+
+        [Column("court_date")]
+        public DateTime CourtDate { get; set; }
+    }
+}
---
+++ Lawyer_app/Views/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Lawyer_app.Services;
+using System.Windows;
+
+namespace Lawyer_app
+{
+    public partial class MainWindow : Window
+    {
+        private readonly DbHelper _dbHelper;
+
+        public MainWindow()
+        {
+            InitializeComponent(); // Важно: должна быть первой строкой!
+            _dbHelper = new DbHelper("Server=localhost;Database=lawyer_app;Uid=root;Pwd=;");
+            LoadTasks();
+        }
+
+        private void LoadTasks()
+        {
+            var tasks = _dbHelper.GetTasks();
+            TasksListView.ItemsSource = tasks; // Имя должно совпадать с x:Name в XAML
+        }
+    }
+}
